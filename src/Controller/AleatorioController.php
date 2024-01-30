f2478610ab26f68a21a70b0c7bc3286e4e95e036
josedamh <josedavidmh@gmail.com>
2024-01-30 14:33:16 +0100

    [+] 30-01-2024

--- src/Controller/AleatorioController.php
+++ src/Controller/AleatorioController.php
@@ -6,16 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/aleatorio', name: 'app_aleatorios')]
 class AleatorioController extends AbstractController
 {
-    #[Route('/aleatorio', name: 'app_aleatorio')]
+    #[Route('/num1', name: 'app_aleatorio')]
     public function index(): Response
     {
         $numAleatorio = random_int(0, 100);
 
         return $this->render('aleatorio/index.html.twig', [
             'controller_name' => 'AleatorioController',
-            'numAleatorio' => $numAleatorio,
+            'numeroAleatorio' => $numAleatorio,
+        ]);
+    }
+
+    #[Route('/num2', name: 'app_aleatorio2')]
+    public function index2(): Response
+    {
+        $numAleatorio = "000";
+
+        return $this->render('aleatorio/index.html.twig', [
+            'controller_name' => 'AleatorioController',
+            'numeroAleatorio' => $numAleatorio,
         ]);
     }
 }
