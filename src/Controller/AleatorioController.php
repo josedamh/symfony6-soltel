c7fb732d0b2404cf999169228a7e843b34b4087e
josedamh <josedavidmh@gmail.com>
2024-01-31 14:21:04 +0100

    [+]Enrutamientos 31-01-2024

--- src/Controller/AleatorioController.php
+++ src/Controller/AleatorioController.php
@@ -6,10 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/aleatorio', name: 'app_aleatorios')]
+// Atributos Symfony6 (#[Route]) y anotaciones Symfony5 (@Route)
+
+#[Route('/num-aleatorios', name: 'app_aleatorio')]
+/**
+ * @Route("/num-aleatorios", name="app_aleatorios")
+ */
 class AleatorioController extends AbstractController
 {
-    #[Route('/num1', name: 'app_aleatorio')]
+    #[Route('/num1', name: 'app_aleatorio1')]
     public function index(): Response
     {
         $numAleatorio = random_int(0, 100);
@@ -24,6 +29,17 @@ class AleatorioController extends AbstractController
     public function index2(): Response
     {
         $numAleatorio = "000";
+
+        return $this->render('aleatorio/index.html.twig', [
+            'controller_name' => 'AleatorioController',
+            'numeroAleatorio' => $numAleatorio,
+        ]);
+    }
+
+    // #[Route('/num3', name: 'app_aleatorio3')]
+    public function index3(): Response
+    {
+        $numAleatorio = "666";
 
         return $this->render('aleatorio/index.html.twig', [
             'controller_name' => 'AleatorioController',
